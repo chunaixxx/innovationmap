b61b45d39d9e4086558780a95d18e862b5cabbed
Daniil <chunaixxx@gmail.com>
2021-04-22 23:56:15 +0300

    ADD: Добавление самописных попапов

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ burgerEl.addEventListener('click', e => {
 	burgerEl.classList.toggle('active');
 	navEl.classList.toggle('active')
 });
+
+const makeModal = modalSel => {
+	const modalEl = document.querySelector(modalSel);
+		  btnEl = document.querySelector(modalSel + '-btn'),
+		  closeEl = document.querySelector(modalSel + ' .modal-wrapper__bg'),
+
+	btnEl.addEventListener('click', () => modalEl.classList.add('active'));
+	closeEl.addEventListener('click', () => modalEl.classList.remove('active'));
+}
+
+makeModal('#modal-1');
